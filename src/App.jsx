3d4093f18396574d43b45da3732da4bf4ef64f49
meow0222 <meow222.csgo@gmail.com>
2023-10-13 08:35:12 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import React from 'react';
+import './App.css';
+import MiniDrawer from './components/MiniDrawer';
+
+function App() {
+
+  return (
+    <>
+    <MiniDrawer/>
+      <div>
+
+      </div>
+    </>
+  );
+};
+export default App;
