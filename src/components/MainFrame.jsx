d33422ca79cf15d2b1ef65779aef888f71f370d8
pinhaosama <gabrielpinhao1@gmail.com>
2023-10-17 18:03:44 -0700

    update GET request test

--- src/components/MainFrame.jsx
+++ src/components/MainFrame.jsx
@@ -19,6 +19,7 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import AccountMenu from './AccountMenu';
+import LandingPage from './LandingPage';
 
 const drawerWidth = 240;
 
