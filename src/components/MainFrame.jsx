a87718827568de818763b5dfa56401880dab62d6
pinhaosama <gabrielpinhao1@gmail.com>
2023-10-18 19:13:40 -0700

    add first version of race dashboard

--- src/components/MainFrame.jsx
+++ src/components/MainFrame.jsx
@@ -19,7 +19,7 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import AccountMenu from './AccountMenu';
-import Dashboard from './Dashboard';
+import Dashboard from './DashBoard';
 import { ScoreResult } from './ScoreResult';
 
 const drawerWidth = 240;
